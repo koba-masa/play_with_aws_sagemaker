5b3dbbe38a9447c0ff8b84a44ddaabddf440a482
koba-masa <kobayashi_mshr@koba-masa.com>
2023-08-08 11:04:38 +0900

    feat: tamtamのコードの検証

--- serverless_inference/src/app.py
+++ serverless_inference/src/app.py
@@ -1,27 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
 
-import glob
-
 app = FastAPI()
 
 @app.get("/ping")
 def ping():
     return {"Hello": "World"}
 
-
 @app.post("/invocations")
 def transformation():
-    opt_ml_mode_dir = "/opt/ml/model"
-    opt_ml_mode_files = {
-        'dir': opt_ml_mode_dir,
-        'files': []
-    }
-    files = glob.glob(f"{opt_ml_mode_dir}/*")
-    for file in files:
-        opt_ml_mode_files['files'].append(file)
-
-    return {"invocated": opt_ml_mode_files}
+    # 推論のコードをここに書きます。
+    return {"invocated": True}
 
 
 if __name__ == "__main__":
